c84628f26ef926c458728d284750f64798620d98
Robert Davidson <davidson.robb001@gmail.com>
2024-06-10 12:14:44 -0400

    module4 completed baseline

---
+++ .seedgooserc.js
@@ -0,0 +1,9 @@
+// .seedgooserc.js
+module.exports = {
+    modelBaseDirectory: "app_server/models",
+    models: ["*.js", "!db.js"],
+    data: "data",
+    db: "mongodb://127.0.0.1:27017/travlr",
+
+};
+
